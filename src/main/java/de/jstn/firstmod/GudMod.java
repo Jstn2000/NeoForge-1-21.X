d1d74be435eda9e7b87d0ea27b85fddc2435c63e
jl228897 <jl228897@fh-muenster.de>
2024-08-09 17:59:30 +0200

    Adding Items

--- src/main/java/de/jstn/firstmod/GudMod.java
+++ src/main/java/de/jstn/firstmod/GudMod.java
@@ -1,5 +1,6 @@
 package de.jstn.firstmod;
 
+import de.jstn.firstmod.item.ModItem;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -33,6 +34,8 @@ public class GudMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModItem.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
---
+++ src/main/java/de/jstn/firstmod/item/ModItem.java
@@ -0,0 +1,19 @@
+package de.jstn.firstmod.item;
+
+
+import de.jstn.firstmod.GudMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class ModItem {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(GudMod.MOD_ID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventbus) {
+        ITEMS.register(eventbus);
+    }
+}
